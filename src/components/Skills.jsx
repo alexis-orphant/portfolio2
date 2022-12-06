684ff9b5061dd5bf583b753c61b3c2a74844f072
Alexis Orphant <alexisorphant@gmail.com>
2022-12-05 23:17:15 -0300

    logo de vite

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import {SiFirebase, SiJavascript, SiTailwindcss} from "react-icons/si"
+import {
+    SiFirebase,
+    SiJavascript,
+    SiTailwindcss,
+    SiVite,
+} from "react-icons/si";
 import { FaCss3Alt, FaGithub, FaHtml5, FaReact } from "react-icons/fa"
 import {DiGit} from "react-icons/di"
 
@@ -11,11 +16,21 @@ const Skills = () => {
                 <h2 className="font-roboto italic text-2xl lg:text-4xl pt-12 lg:pt-4">
                     Habilidades Técnicas
                 </h2>
-                <ul className="grid  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 mt-6 lg:mt-10">
+                <ul className="items-center grid  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 mt-6 lg:mt-10">
                     <li className="pt-4">
                         <FaReact size={80} className="text-[#61DBFB] mx-auto" />
                         <h3 className="font-roboto text-xl text-center">
                             REACT JS
+                        </h3>
+                    </li>
+                    <li className="pt-4">
+                        <img
+                            src="https://vitejs.dev/logo-with-shadow.png"
+                            alt="vite js logo"
+                            className="w-20 mx-auto"
+                        />
+                        <h3 className="font-roboto text-xl text-center">
+                            Vite
                         </h3>
                     </li>
                     <li className="pt-4">
@@ -71,9 +86,7 @@ const Skills = () => {
                     </li>
                     <li className="pt-4">
                         <DiGit size={80} className="text-[#f34f29] mx-auto" />
-                        <h3 className="font-roboto text-xl text-center">
-                            Git
-                        </h3>
+                        <h3 className="font-roboto text-xl text-center">Git</h3>
                     </li>
                 </ul>
             </main>
